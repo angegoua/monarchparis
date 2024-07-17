4924eef9653f1acbaa333477eca90a356591c315
angegoua <ange.goua01@gmail.com>
2024-07-17 02:01:48 +0200

    :lipstick: rethink btn and create li icon

--- app/components/Button.tsx
+++ app/components/Button.tsx
@@ -10,10 +10,11 @@ export const Button = ({ children, variant }: ButtonProps) => {
   return (
     <button
       className={clsx(
-        'py-2 px-6 relative before:inset-0 before:absolute  before:bg-gradient-to-b  before:to-transparent rounded-full before:rounded-full before:opacity-0 hover:before:opacity-10 before:transition-opacity before:duration-300',
-        variant === 'primary' && 'text-white bg-orange before:from-white',
+        'py-2 px-6 relative font-medium font-display',
+        variant === 'primary' &&
+          'text-black  bg-gradient-to-b from-white to-[#E1E1E1] rounded-full hover:bg-gradient-to-t transition-all duration-300',
         variant === 'secondary' &&
-          'bg-white text-black border-gray-200 border before:from-gray-500'
+          'bg-white text-black border-gray-200 border before:from-gray-500 before:inset-0 before:absolute before:bg-gradient-to-b  before:to-transparent rounded-full before:rounded-full before:opacity-0 hover:before:opacity-10 before:transition-opacity before:duration-300'
       )}
     >
       {children}
