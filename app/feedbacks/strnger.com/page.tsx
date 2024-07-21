026334d77aedb9f452994b489f11f04f7f001aa6
angegoua <ange.goua01@gmail.com>
2024-07-21 02:52:43 +0200

    feat: add StrngerRedirection component for redirecting to Strnger feedback page

---
+++ app/feedbacks/strnger.com/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation'
+
+export default async function StrngerRedirection() {
+  return redirect(
+    'https://debonair-robin-f39.notion.site/Feedback-on-Strnger-s-landing-page-21cf022fa3954ead87eb82f59ddb4830'
+  )
+}
